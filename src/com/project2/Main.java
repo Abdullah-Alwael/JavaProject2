2b4653a2d11c9abe011481b66484113fc5e30de3
Abdullah-Alwael <abdullah.alwael@outlook.sa>
2025-07-14 11:54:43 +0300

    TODO

--- src/com/project2/Main.java
+++ src/com/project2/Main.java
@@ -2,6 +2,19 @@ package com.project2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello project 2");
+        /*TODO
+            • Create 2 routes and assign each of them to a different Car object. Note that one
+                of those cars should have a maximum capacity equal to zero .
+        */
+
+        /*TODO
+           • Create an array of Passengers containing 1 Subscriber and 1 non-Subscriber.
+        /*
+
+        /*TODO
+           • Assign a different car object to each passenger and then display their information.
+         */
+
+
     }
 }
